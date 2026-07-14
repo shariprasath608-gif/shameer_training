f4ecdd1106b2c756ea91951fc7cb6b86a8dc2520
Hari_608 <shariprasath608@gmail.com>
2026-07-14 20:27:15 +0530

    Add Product_companies important leetcode problems

---
+++ Baktracking/maxPrint.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class maxPrint {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        backtrack(0, board, result);
+        return result;
+    }
+
+    private void backtrack(int row, char[][] board, List<List<String>> result) {
+        if (row == board.length) {
+            result.add(construct(board));
+            return;
+        }
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(row + 1, board, result);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') return false;
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') return false;
+        }
+        return true;
+    }
+
+    private List<String> construct(char[][] board) {
+        List<String> res = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            res.add(new String(board[i]));
+        }
+        return res;
+    }
+}
